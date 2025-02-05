29cb5cc0d5cd0fa21b2f197e32183b3b7a901453
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-05 13:39:47 +0330

    style added and navbar moved to header folder

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,4 +1,4 @@
-import NavBar from "../NavBar/NavBar"
+import NavBar from "./NavBar/NavBar"
 import "./Header.css"
 export default function Header() {
   return (
@@ -7,15 +7,22 @@ export default function Header() {
 
        <NavBar/> 
 
-    <div className="container header_container">
+    <div className=" header_container">
 
-    <div className="title_container">
+   <div className="container  herder_wrapper">
+   <div className="title_container">
     <p className="Header_title">NADER IRANIAN ARCHITECTURE COMPANY</p>
     </div>
 
-    <div className="header_info">
+    
+   </div>
+   <div className="header_info ">
 
-
+        <div className="fist_box">
+            <p className="years">10+</p>
+            <span className="tears_text"> YEARS OF EXPERIENCE</span>
+        </div>
+        
     </div>
 
     </div>
