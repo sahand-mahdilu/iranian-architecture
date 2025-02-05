23e1d521d3ece11f6b030e170433a837c46119ae
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-05 19:42:35 +0330

    feat : footer style added

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import "./Footer.css"
+import FooterBox from "./FooterComponent/FooterBox"
 export default function Footer() {
   return (
     <div className="footer_container">
@@ -6,7 +7,21 @@ export default function Footer() {
             <p className="footer_first_Text">Ways of communication</p>
             <p className="footer_cesond_Text">Ways to contact the rare Iranian architecture</p>
         </div>
-        <div className="footer_box_container"></div>
+        <div className="footer_box_container">
+        <FooterBox/>
+        <FooterBox/>
+        <FooterBox/>
+
+        </div>
+
+        <div className="footer_info">
+            
+                <input  type="text" placeholder="Name" />
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Phone" />
+                <button className="send">Send</button>            
+
+        </div>
 
 
     </div>
--- src/Footer/FooterComponent/FooterBox.jsx
+++ src/Footer/FooterComponent/FooterBox.jsx
@@ -1,6 +1,18 @@
-
+import "./FooterBox.css"
 export default function FooterBox() {
   return (
-    <div>FooterBox</div>
+    <div className="footer_box">
+
+
+<img className="phone" src="./public/img/8748459.png" alt="" />
+
+
+    <p className="phone_text" >PHONE</p>
+    <span className="fnum">091200550</span>
+    <span className="secNum">093788550</span>
+
+
+
+    </div>
   )
 }
