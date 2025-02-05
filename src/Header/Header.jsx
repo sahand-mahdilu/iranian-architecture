608123c9ebd1d5d33035e3c55386422aacc26f96
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-05 12:28:40 +0330

    feat: navbar created

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,7 +3,14 @@ export default function Header() {
   return (
     <div className="container header_container">
 
-    <p>NADER IRANIAN ARCHITECTURE COMPANY</p>
+    <div className="title_container">
+    <p className="Header_title">NADER IRANIAN ARCHITECTURE COMPANY</p>
+    </div>
+
+    <div className="header_info">
+    
+
+    </div>
 
     </div>
   )
